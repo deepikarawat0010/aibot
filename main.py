4394d2511497483b9c0a92eb450755b2e01d0a39
Deepika Rawat <deepikarawat.0010@gmail.com>
2026-06-15 03:05:15 +0530

    AI Career Coach

---
+++ main.py
@@ -0,0 +1,30 @@
+#ARTIFICIAL INTELLIGENCE CHATBOT - AI BUILDERS BOOTCAMP BY VEDAM SCHOOL OF TECHNOLOGY
+import streamlit as st
+import ollama
+
+st.title("AI Career Coach")
+
+question = st.text_input("Ask your career question")
+
+if st.button("Submit") and question:
+
+    response = ollama.chat(
+        model="qwen2.5:0.5b",
+        messages=[
+            {
+                "role": "system",
+                "content": """You are an expert career coach.
+                Help students with:
+                - Career choices
+                - Colleges
+                - Skills
+                - Internships"""
+            },
+            {
+                "role": "user",
+                "content": question
+            }
+        ]
+    )
+
+    st.write(response["message"]["content"])
